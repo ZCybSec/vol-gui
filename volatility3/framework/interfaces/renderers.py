45ee399623814b7737962bebc7ea89f1546fa1dd
Mike Auty <mike.auty@gmail.com>
2025-02-09 22:18:25 +0000

    Core: Fix up yet another typo

--- volatility3/framework/interfaces/renderers.py
+++ volatility3/framework/interfaces/renderers.py
@@ -58,7 +58,7 @@ class TypeRendererInterface:
     def options(self):
         return self._options
 
-    def render(self, data: Union[T,BaseAbsentValue] -> Any:
+    def render(self, data: Union[T,BaseAbsentValue]) -> Any:
         """Renders a specific datatype"""
         return ""
 
