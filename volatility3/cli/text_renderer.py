e936b33784138e4f0916b61ea1edc555d2fd5f39
Mike Auty <mike.auty@gmail.com>
2025-02-15 21:52:15 +0000

    CLI: Fix ruff check error

--- volatility3/cli/text_renderer.py
+++ volatility3/cli/text_renderer.py
@@ -1,5 +1,3 @@
-from volatility3.framework.interfaces.layers import TranslationLayerInterface
-
 # This file is Copyright 2019 Volatility Foundation and licensed under the Volatility Software License 1.0
 # which is available at https://www.volatilityfoundation.org/license/vsl-v1.0
 #
