9fe4092e9f9c18ddb6be452faecba476496dfbf1
Andrew Case <andrew@dfir.org>
2025-03-07 03:34:26 +0000

    Update the list_threads API to current standards and update current callers to new form

--- volatility3/framework/plugins/windows/suspended_threads.py
+++ volatility3/framework/plugins/windows/suspended_threads.py
@@ -36,7 +36,7 @@ class SuspendedThreads(interfaces.plugins.PluginInterface):
                 name="pe_symbols", component=pe_symbols.PESymbols, version=(2, 0, 0)
             ),
             requirements.VersionRequirement(
-                name="threads", component=threads.Threads, version=(2, 0, 0)
+                name="threads", component=threads.Threads, version=(3, 0, 0)
             ),
         ]
 
@@ -54,8 +54,6 @@ class SuspendedThreads(interfaces.plugins.PluginInterface):
 
         https://www.volexity.com/wp-content/uploads/2024/08/Defcon24_EDR_Evasion_Detection_White-Paper_Andrew-Case.pdf
         """
-        kernel = self.context.modules[self.config["kernel"]]
-
         vads_cache: Dict[int, pe_symbols.PESymbols.ranges_type] = {}
 
         proc_modules = None
@@ -64,7 +62,9 @@ class SuspendedThreads(interfaces.plugins.PluginInterface):
         for proc in pslist.PsList.list_processes(
             context=self.context, kernel_module_name=self.config["kernel"]
         ):
-            for thread in threads.Threads.list_threads(kernel, proc):
+            for thread in threads.Threads.list_threads(
+                self.context, self.config["kernel"], proc
+            ):
                 try:
                     # we only care if the thread is suspended
                     if thread.Tcb.SuspendCount == 0:
--- volatility3/framework/plugins/windows/suspicious_threads.py
+++ volatility3/framework/plugins/windows/suspicious_threads.py
@@ -41,7 +41,7 @@ class SuspiciousThreads(interfaces.plugins.PluginInterface):
                 name="pslist", component=pslist.PsList, version=(3, 0, 0)
             ),
             requirements.VersionRequirement(
-                name="threads", component=threads.Threads, version=(2, 0, 0)
+                name="threads", component=threads.Threads, version=(3, 0, 0)
             ),
             requirements.VersionRequirement(
                 name="vadinfo", component=vadinfo.VadInfo, version=(2, 0, 0)
@@ -169,7 +169,9 @@ class SuspiciousThreads(interfaces.plugins.PluginInterface):
             # there is no benefit to checking the same address more than once per process
             checked = set()
 
-            for thread in threads.Threads.list_threads(kernel, proc):
+            for thread in threads.Threads.list_threads(
+                self.context, self.config["kernel"], proc
+            ):
                 # do not process if a thread is exited or terminated (4 = Terminated)
                 if thread.ExitTime.QuadPart > 0 or thread.Tcb.State == 4:
                     continue
